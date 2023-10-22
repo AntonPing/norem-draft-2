b2245b5a2c168f5301ef5ae7947f571c5ef13ad7
Anton Ping <antonping1999@gmail.com>
2023-10-22 13:15:11 +0300

    dev: define a simple ast

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+pub mod syntax;
+
 fn main() {
     println!("Hello, world!");
 }
---
+++ src/syntax/ast.rs
@@ -0,0 +1,76 @@
+type Ident = String;
+type Span = (usize, usize);
+
+#[derive(Clone, Copy, Debug, PartialEq, PartialOrd)]
+pub enum LitVal {
+    Int(i64),
+    Float(f64),
+    Bool(bool),
+    Char(char),
+    Unit,
+}
+
+#[derive(Clone, Copy, Debug, Hash, Eq, PartialEq, PartialOrd)]
+pub enum LitType {
+    TyInt,
+    TyFloat,
+    TyBool,
+    TyChar,
+    TyUnit,
+}
+
+impl LitVal {
+    pub fn get_lit_type(&self) -> LitType {
+        match self {
+            LitVal::Int(_) => LitType::TyInt,
+            LitVal::Float(_) => LitType::TyFloat,
+            LitVal::Bool(_) => LitType::TyBool,
+            LitVal::Char(_) => LitType::TyChar,
+            LitVal::Unit => LitType::TyUnit,
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub enum PrimOpr {
+    IAdd,
+    ISub,
+    IMul,
+}
+
+impl PrimOpr {
+    pub fn get_arity(&self) -> usize {
+        match self {
+            PrimOpr::IAdd => 2,
+            PrimOpr::ISub => 2,
+            PrimOpr::IMul => 2,
+        }
+    }
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum Expr {
+    Lit {
+        lit: LitVal,
+        span: Span,
+    },
+    Var {
+        ident: Ident,
+        span: Span,
+    },
+    Prim {
+        prim: PrimOpr,
+        args: Vec<Expr>,
+        span: Span,
+    },
+    Func {
+        pars: Vec<Ident>,
+        body: Box<Expr>,
+        span: Span,
+    },
+    App {
+        func: Box<Expr>,
+        args: Vec<Expr>,
+        span: Span,
+    },
+}
