5a2bf0cafc550ad106094795773b54e0559f007f
Anton Ping <antonping1999@gmail.com>
2024-01-17 12:32:52 +0300

    dev: implement type checker for new syntax

--- src/typing/check.rs
+++ src/typing/check.rs
@@ -21,7 +21,7 @@ impl TypeChecker {
 type CheckResult<T> = Result<T, String>;
 
 impl TypeChecker {
-    fn infer_expr(&mut self, expr: &Expr) -> CheckResult<UnifyType> {
+    fn check_expr(&mut self, expr: &Expr) -> CheckResult<UnifyType> {
         match expr {
             Expr::Lit { lit, span: _ } => {
                 let typ = UnifyType::Lit(lit.get_lit_type());
@@ -44,14 +44,14 @@ impl TypeChecker {
                 }
                 match prim {
                     PrimOpr::IAdd | PrimOpr::ISub | PrimOpr::IMul => {
-                        let arg0 = self.infer_expr(&args[0])?;
+                        let arg0 = self.check_expr(&args[0])?;
                         self.solver.unify(&arg0, &UnifyType::Lit(LitType::TyInt))?;
-                        let arg1 = self.infer_expr(&args[1])?;
+                        let arg1 = self.check_expr(&args[1])?;
                         self.solver.unify(&arg1, &UnifyType::Lit(LitType::TyInt))?;
                         Ok(UnifyType::Lit(LitType::TyInt))
                     }
                     PrimOpr::Move => {
-                        let arg0 = self.infer_expr(&args[0])?;
+                        let arg0 = self.check_expr(&args[0])?;
                         Ok(arg0)
                     }
                 }
@@ -69,7 +69,7 @@ impl TypeChecker {
                         cell
                     })
                     .collect();
-                let body_ty = Box::new(self.infer_expr(body)?);
+                let body_ty = Box::new(self.check_expr(body)?);
                 Ok(UnifyType::Func(pars_ty, body_ty))
             }
             Expr::App {
@@ -77,10 +77,10 @@ impl TypeChecker {
                 args,
                 span: _,
             } => {
-                let func_ty = self.infer_expr(func)?;
+                let func_ty = self.check_expr(func)?;
                 let args_ty = args
                     .iter()
-                    .map(|arg| self.infer_expr(arg))
+                    .map(|arg| self.check_expr(arg))
                     .collect::<CheckResult<Vec<_>>>()?;
                 let res_ty = self.solver.new_cell();
                 self.solver.unify(
@@ -100,41 +100,94 @@ impl TypeChecker {
                     expr,
                     span: _,
                 } => {
-                    let ty = self.infer_expr(expr)?;
+                    let ty = self.check_expr(expr)?;
                     if let Some(ty_anno) = ty_anno {
                         self.solver.unify(&ty, &ty_anno.into())?;
                     }
                     self.context.insert(*ident, ty);
-                    let res_ty = self.infer_expr(cont)?;
+                    let res_ty = self.check_expr(cont)?;
                     Ok(res_ty)
                 }
                 Stmt::Do { expr, span: _ } => {
-                    let ty = self.infer_expr(expr)?;
+                    let ty = self.check_expr(expr)?;
                     self.solver.unify(&ty, &UnifyType::Lit(LitType::TyUnit))?;
-                    let res_ty = self.infer_expr(cont)?;
+                    let res_ty = self.check_expr(cont)?;
                     Ok(res_ty)
                 }
             },
         }
     }
+
+    fn check_decl(&mut self, decl: &Decl) -> CheckResult<()> {
+        match decl {
+            Decl::Func {
+                pars, res, body, ..
+            } => {
+                for (par, typ) in pars {
+                    self.context.insert(*par, typ.into());
+                }
+                let res_ty = self.check_expr(body)?;
+                self.solver.unify(&res_ty, &res.into())?;
+                Ok(())
+            }
+        }
+    }
+
+    fn check_module(&mut self, modl: &Module) -> CheckResult<()> {
+        let Module { name: _, decls } = modl;
+        for decl in decls {
+            match decl {
+                Decl::Func {
+                    func, pars, res, ..
+                } => {
+                    let (_, par_tys): (Vec<Ident>, Vec<UnifyType>) =
+                        pars.iter().map(|(par, typ)| (par, typ.into())).unzip();
+                    let res_ty: UnifyType = res.into();
+                    let func_ty = UnifyType::Func(par_tys, Box::new(res_ty));
+                    self.context.insert(*func, func_ty);
+                }
+            }
+        }
+        for decl in decls {
+            self.check_decl(decl)?;
+        }
+        Ok(())
+    }
 }
 
-pub fn expr_infer(expr: &Expr) -> CheckResult<UnifyType> {
+pub fn check_expr(expr: &Expr) -> CheckResult<UnifyType> {
     let mut pass = TypeChecker::new();
-    let res = pass.infer_expr(expr)?;
+    let res = pass.check_expr(expr)?;
     Ok(pass.solver.merge(&res))
+}
+
+pub fn check_module(modl: &Module) -> CheckResult<()> {
+    let mut pass = TypeChecker::new();
+    pass.check_module(modl)?;
+    Ok(())
 }
 
 #[test]
 #[ignore = "just to see result"]
 fn check_test() {
-    use crate::syntax::parser::parse_expr;
-    use crate::syntax::rename::rename_expr;
     let s = r#"
-fn(f) => fn(g) => fn(x) => (f(x))(g(x))
+module test
+begin
+    function f(x: Int) -> Int
+    begin
+        let h = fn(x) => @iadd(x, 1);
+        let r = g(x);
+        r
+    end
+    function g(x: Int) -> Int
+    begin
+        let r = @iadd(x, 1);
+        r
+    end
+end
 "#;
-    let mut res = parse_expr(s).unwrap();
-    rename_expr(&mut res).unwrap();
-    let ty = expr_infer(&res).unwrap();
-    println!("{:#?}", ty);
+
+    let mut modl = crate::syntax::parser::parse_module(s).unwrap();
+    crate::syntax::rename::rename_module(&mut modl).unwrap();
+    check_module(&modl).unwrap();
 }
