fa64b15d3f61b8b2126672c870696e97e0c6fb92
Anton Ping <antonping1999@gmail.com>
2024-02-25 18:14:39 +0300

    dev: implement unify for constructors

--- src/typing/unify.rs
+++ src/typing/unify.rs
@@ -6,8 +6,9 @@ use std::ops::Deref;
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub enum UnifyType {
     Lit(LitType),
-    Func(Vec<UnifyType>, Box<UnifyType>),
     Var(Ident),
+    Cons(Ident, Vec<UnifyType>),
+    Func(Vec<UnifyType>, Box<UnifyType>),
     Cell(usize),
 }
 
@@ -16,7 +17,10 @@ impl From<&Type> for UnifyType {
         match value {
             Type::Lit { lit } => UnifyType::Lit(*lit),
             Type::Var { ident } => UnifyType::Var(*ident),
-            Type::Cons { cons, args } => todo!(),
+            Type::Cons { cons, args } => {
+                let args = args.iter().map(|arg| arg.into()).collect();
+                UnifyType::Cons(*cons, args)
+            }
             Type::Func { pars, res } => {
                 let pars = pars.iter().map(|par| par.into()).collect();
                 let res = Box::new(res.deref().into());
@@ -42,19 +46,28 @@ impl UnifySolver {
         UnifyType::Cell(self.arena.len() - 1)
     }
 
+    pub fn unify_many(&mut self, typs1: &Vec<UnifyType>, typs2: &Vec<UnifyType>) -> UnifyResult {
+        if typs1.len() != typs2.len() {
+            Err("Can't unify two type vectors with different length!".to_string())
+        } else {
+            for (typ1, typ2) in typs1.iter().zip(typs2.iter()) {
+                self.unify(typ1, typ2)?;
+            }
+            Ok(())
+        }
+    }
+
     pub fn unify(&mut self, typ1: &UnifyType, typ2: &UnifyType) -> UnifyResult {
         match (typ1, typ2) {
             (UnifyType::Lit(lit1), UnifyType::Lit(lit2)) if lit1 == lit2 => Ok(()),
             (UnifyType::Var(ident1), UnifyType::Var(ident2)) if ident1 == ident2 => Ok(()),
+            (UnifyType::Cons(cons1, args1), UnifyType::Cons(cons2, args2)) if cons1 == cons2 => {
+                self.unify_many(args1, args2)?;
+                Ok(())
+            }
+
             (UnifyType::Func(pars1, res1), UnifyType::Func(pars2, res2)) => {
-                if pars1.len() != pars2.len() {
-                    return Err(
-                        "Can't unify two functions with different parameter length!".to_string()
-                    );
-                }
-                for (par1, par2) in pars1.iter().zip(pars2.iter()) {
-                    self.unify(par1, par2)?;
-                }
+                self.unify_many(pars1, pars2)?;
                 self.unify(res1, res2)?;
                 Ok(())
             }
@@ -81,6 +94,7 @@ impl UnifySolver {
         match typ {
             UnifyType::Lit(_) => false,
             UnifyType::Var(_) => false,
+            UnifyType::Cons(_, args) => args.iter().all(|arg| self.occur_check(cell, arg)),
             UnifyType::Func(pars, res) => pars
                 .iter()
                 .chain(std::iter::once(res.deref()))
@@ -98,6 +112,10 @@ impl UnifySolver {
 
     pub fn merge(&self, typ: &UnifyType) -> UnifyType {
         match typ {
+            UnifyType::Cons(cons, args) => {
+                let args = args.iter().map(|arg| self.merge(arg)).collect();
+                UnifyType::Cons(*cons, args)
+            }
             UnifyType::Func(pars, res) => {
                 let pars = pars.iter().map(|par| self.merge(par)).collect();
                 let res = Box::new(self.merge(res));
@@ -115,38 +133,47 @@ impl UnifySolver {
     }
 
     pub fn instantiate(&mut self, polys: &Vec<Ident>, typ: &UnifyType) -> UnifyType {
-        fn instantiate_aux(map: &HashMap<Ident, usize>, typ: &UnifyType) -> UnifyType {
-            match typ {
-                UnifyType::Lit(lit) => UnifyType::Lit(*lit),
-                UnifyType::Func(pars, res) => {
-                    let pars = pars.iter().map(|par| instantiate_aux(map, par)).collect();
-                    let res = Box::new(instantiate_aux(map, res));
-                    UnifyType::Func(pars, res)
-                }
-                UnifyType::Var(ident) => {
-                    if let Some(cell) = map.get(ident) {
-                        UnifyType::Cell(*cell)
-                    } else {
-                        UnifyType::Var(*ident)
-                    }
-                }
-                UnifyType::Cell(cell) => UnifyType::Cell(*cell),
-            }
-        }
         if polys.is_empty() {
             typ.clone()
         } else {
-            let map = polys
-                .iter()
-                .map(|poly| {
-                    if let UnifyType::Cell(cell) = self.new_cell() {
-                        (*poly, cell)
-                    } else {
-                        unreachable!()
-                    }
-                })
-                .collect();
-            instantiate_aux(&map, typ)
+            let map = self.make_instantiate_map(polys);
+            substitute(&map, typ)
         }
+    }
+
+    pub fn make_instantiate_map(&mut self, polys: &Vec<Ident>) -> HashMap<Ident, usize> {
+        polys
+            .iter()
+            .map(|poly| {
+                if let UnifyType::Cell(cell) = self.new_cell() {
+                    (*poly, cell)
+                } else {
+                    unreachable!()
+                }
+            })
+            .collect()
+    }
+}
+
+pub fn substitute(map: &HashMap<Ident, usize>, typ: &UnifyType) -> UnifyType {
+    match typ {
+        UnifyType::Lit(lit) => UnifyType::Lit(*lit),
+        UnifyType::Var(ident) => {
+            if let Some(cell) = map.get(ident) {
+                UnifyType::Cell(*cell)
+            } else {
+                UnifyType::Var(*ident)
+            }
+        }
+        UnifyType::Cons(cons, args) => {
+            let args = args.iter().map(|arg| substitute(map, arg)).collect();
+            UnifyType::Cons(*cons, args)
+        }
+        UnifyType::Func(pars, res) => {
+            let pars = pars.iter().map(|par| substitute(map, par)).collect();
+            let res = Box::new(substitute(map, res));
+            UnifyType::Func(pars, res)
+        }
+        UnifyType::Cell(cell) => UnifyType::Cell(*cell),
     }
 }
