beaf603315a55db8408c3ca0a131e0c9b2e50244
Anton Ping <antonping1999@gmail.com>
2024-02-27 20:24:30 +0300

    dev: implement pattern match in normalizer

--- src/optimize/normalize.rs
+++ src/optimize/normalize.rs
@@ -1,11 +1,19 @@
+use std::collections::HashMap;
 use std::iter;
 use std::ops::Deref;
 
 use super::anf::{self, Atom, PrimOpr};
-use crate::syntax::ast::{self, Module};
-use crate::utils::ident::Ident;
+use super::pattern::PatnMatrix;
 
-pub struct Normalizer {}
+use crate::optimize::pattern;
+use crate::syntax::ast::{self, Pattern, Varient};
+use crate::utils::ident::Ident;
+use crate::utils::intern::InternStr;
+
+pub struct Normalizer {
+    cons_map: HashMap<Ident, Ident>,
+    data_map: HashMap<Ident, Vec<Varient>>,
+}
 
 fn subst(expr: anf::Expr, hole: Ident, atom: Atom) -> anf::Expr {
     // subst(expr,hole,atom) ~=~ let hole = move(atom); expr
@@ -20,12 +28,46 @@ fn subst(expr: anf::Expr, hole: Ident, atom: Atom) -> anf::Expr {
 
 impl Normalizer {
     pub fn run(modl: &ast::Module) -> anf::Module {
-        let mut pass = Normalizer {};
-        pass.normalize_module(modl)
+        let mut pass = Normalizer {
+            cons_map: HashMap::new(),
+            data_map: HashMap::new(),
+        };
+        let mut modl = pass.normalize_module(modl);
+        super::rename::Renamer::run(&mut modl);
+        modl
+    }
+
+    pub fn normalize_module(&mut self, modl: &ast::Module) -> anf::Module {
+        let ast::Module { name, decls } = modl;
+        for decl in decls.iter() {
+            match decl {
+                ast::Decl::Func { .. } => {}
+                ast::Decl::Data {
+                    ident,
+                    polys: _,
+                    vars,
+                    span: _,
+                } => {
+                    self.data_map.insert(*ident, vars.clone());
+                    for var in vars {
+                        self.cons_map.insert(var.cons, *ident);
+                    }
+                }
+            }
+        }
+        let decls = decls
+            .iter()
+            .flat_map(|decl| self.normalize_decl(decl))
+            .collect();
+
+        anf::Module { name: *name, decls }
     }
 
     pub fn normalize_expr(expr: &ast::Expr) -> anf::Expr {
-        let mut pass = Normalizer {};
+        let mut pass = Normalizer {
+            cons_map: HashMap::new(),
+            data_map: HashMap::new(),
+        };
         let bind = Ident::fresh(&"r");
         pass.normalize_expr_with_cont(
             expr,
@@ -88,7 +130,50 @@ impl Normalizer {
                 flds,
                 span: _,
             } => {
-                todo!()
+                // normalize(ci(e1,..,en), hole, ctx) =
+                // normalize(en,xn,
+                //   ...
+                //     normalize(e1,x1,
+                //       normalize(e0,f,
+                //         let r = @record(x0, ..., xn);
+                //         let hole = move(r);
+                //         ctx))...)
+
+                let typ = self.cons_map[cons];
+                let (tag, label_vec): (usize, Vec<InternStr>) = self.data_map[&typ]
+                    .iter()
+                    .enumerate()
+                    .find(|(_, var)| var.cons == *cons)
+                    .map(|(i, var)| (i, var.flds.iter().map(|fld| fld.label).collect()))
+                    .unwrap();
+
+                let xs: Vec<Ident> = label_vec.iter().map(|_| Ident::fresh(&"x")).collect();
+                let r = Ident::fresh(&"r");
+                let cont = anf::Expr::Prim {
+                    bind: r,
+                    prim: anf::PrimOpr::Record,
+                    args: [Atom::Int(tag as i64)]
+                        .into_iter()
+                        .chain(xs.iter().map(|x| Atom::Var(*x)))
+                        .collect(),
+                    cont: Box::new(anf::Expr::Prim {
+                        bind: *bind,
+                        prim: anf::PrimOpr::Move,
+                        args: vec![Atom::Var(r)],
+                        cont: Box::new(rest),
+                    }),
+                };
+
+                label_vec
+                    .iter()
+                    .zip(xs.iter())
+                    .map(|(label, bind)| {
+                        let fld = flds.iter().find(|fld| fld.label == *label).unwrap();
+                        (bind, &fld.data)
+                    })
+                    .fold(cont, |cont, (bind, expr)| {
+                        self.normalize_expr_with_cont(&expr, bind, cont)
+                    })
             }
             ast::Expr::App {
                 func,
@@ -176,7 +261,83 @@ impl Normalizer {
                 rules,
                 span: _,
             } => {
-                todo!()
+                /*
+                    normalize(
+                        match top of
+                        | patn_0 => e_0
+                        ......
+                        | patn_n => e_n
+                    hole,
+                    rest
+                    ) =
+                    normalize(top, o
+                        decls
+                            fn a_0(..) begin normalize(e_0, r, j(r) end
+                            ......
+                            fn a_n(..) begin normalize(e_n, r, j(r) end
+                            fn j(hole) begin rest end
+                        in
+                            inline(
+                                compile_match_with(
+                                    (o),
+                                    (patn_1)
+                                    ......
+                                    (patn_n),
+                                    (a_1, ..., a_n),
+                                ),
+                            )
+                        end
+                    )
+                */
+
+                let j = Ident::fresh(&"j");
+                let r = Ident::fresh(&"r");
+
+                let obj = Ident::fresh(&"o");
+                let objs = vec![obj];
+
+                let mut decls: Vec<anf::Decl> = Vec::new();
+                let (matrix, acts): (Vec<Vec<Pattern>>, Vec<Ident>) = rules
+                    .iter()
+                    .map(|rule| {
+                        let act = Ident::fresh(&"a");
+                        let binds = pattern::get_bind_vars(&rule.patn);
+                        let body = self.normalize_expr_with_cont(
+                            &rule.body,
+                            &r,
+                            anf::Expr::Call {
+                                bind: r,
+                                func: Atom::Var(j),
+                                args: vec![Atom::Var(r)],
+                                cont: Box::new(anf::Expr::Retn { res: Atom::Var(r) }),
+                            },
+                        );
+                        let decl = anf::Decl {
+                            func: act,
+                            pars: binds,
+                            body,
+                            info: anf::CallInfo::JoinPoint,
+                        };
+                        decls.push(decl);
+                        (vec![rule.patn.clone()], act)
+                    })
+                    .unzip();
+
+                let decl = anf::Decl {
+                    func: j,
+                    pars: vec![*bind],
+                    body: rest,
+                    info: anf::CallInfo::JoinPoint,
+                };
+                decls.push(decl);
+
+                let mat = PatnMatrix { objs, matrix, acts };
+                let cont = self.normalize_match(&mat, &decls);
+                let rest = anf::Expr::Decls {
+                    decls,
+                    cont: Box::new(cont),
+                };
+                self.normalize_expr_with_cont(expr, &obj, rest)
             }
             ast::Expr::Stmt { stmt, cont, .. } => {
                 // normalize(let x = e1; e2, hole, ctx) =
@@ -197,7 +358,7 @@ impl Normalizer {
             }
         }
     }
-    pub fn normalize_decl(&mut self, decl: &ast::Decl) -> anf::Decl {
+    pub fn normalize_decl(&mut self, decl: &ast::Decl) -> Option<anf::Decl> {
         match decl {
             ast::Decl::Func {
                 ident,
@@ -217,29 +378,115 @@ impl Normalizer {
                         res: Atom::Var(bind),
                     },
                 );
-                anf::Decl {
+                Some(anf::Decl {
                     func: *ident,
                     pars,
                     body,
                     info: anf::CallInfo::NoInfo,
-                }
+                })
             }
-            ast::Decl::Data {
-                ident,
-                polys,
-                vars,
-                span: _,
-            } => {
-                todo!()
-            }
+            ast::Decl::Data { .. } => None,
         }
     }
-    pub fn normalize_module(&mut self, modl: &ast::Module) -> anf::Module {
-        let Module { name, decls } = modl;
 
-        let decls = decls.iter().map(|decl| self.normalize_decl(decl)).collect();
+    fn normalize_match(&mut self, mat: &PatnMatrix, decls: &Vec<anf::Decl>) -> anf::Expr {
+        if mat.is_empty() {
+            panic!("pattern match not exhaustive!")
+        } else if mat.first_row_aways_match() {
+            let (bindings, act) = mat.success();
+            let r = Ident::fresh(&"r");
+            bindings.into_iter().fold(
+                anf::Expr::Call {
+                    bind: r,
+                    func: Atom::Var(act),
+                    args: decls
+                        .iter()
+                        .find(|decl| decl.func == act)
+                        .unwrap()
+                        .pars
+                        .iter()
+                        .map(|par| Atom::Var(*par))
+                        .collect(),
+                    cont: Box::new(anf::Expr::Retn { res: Atom::Var(r) }),
+                },
+                |cont, (var, obj)| anf::Expr::Prim {
+                    bind: var,
+                    prim: PrimOpr::Move,
+                    args: vec![Atom::Var(obj)],
+                    cont: Box::new(cont),
+                },
+            )
+        } else {
+            let j = 0;
+            match mat.get_col_type(j) {
+                pattern::ColType::Lit => {
+                    todo!()
+                }
+                pattern::ColType::Cons => {
+                    let cons_set = mat.get_cons_set(j);
+                    let typ = self.cons_map[&cons_set[0]];
+                    let vars = self.data_map[&typ].clone();
+                    for cons in cons_set {
+                        assert!(vars.iter().any(|var| var.cons == cons));
+                    }
+                    let mut brchs: Vec<anf::Expr> = Vec::new();
+                    for var in vars {
+                        let binds = var
+                            .flds
+                            .iter()
+                            .map(|fld| (fld.label, Ident::fresh(&"o")))
+                            .collect();
+                        let (new_mat, hits) = mat.specialize_cons(j, &var.cons, &binds);
+                        println!("hits: {:?}", hits);
 
-        anf::Module { name: *name, decls }
+                        let brch = binds.iter().enumerate().fold(
+                            self.normalize_match(&new_mat, decls),
+                            |cont, (i, (_label, obj))| anf::Expr::Prim {
+                                bind: *obj,
+                                prim: PrimOpr::Select,
+                                args: vec![Atom::Var(mat.objs[j]), Atom::Int((i + 1) as i64)],
+                                cont: Box::new(cont),
+                            },
+                        );
+
+                        let brch = hits.iter().fold(brch, |cont, hit| anf::Expr::Prim {
+                            bind: *hit,
+                            prim: PrimOpr::Move,
+                            args: vec![Atom::Var(mat.objs[j])],
+                            cont: Box::new(cont),
+                        });
+                        brchs.push(brch);
+                    }
+
+                    let t = Ident::fresh(&"t");
+                    anf::Expr::Prim {
+                        bind: t,
+                        prim: PrimOpr::Select,
+                        args: vec![Atom::Var(mat.objs[j]), Atom::Int(0)],
+                        cont: Box::new(anf::Expr::Brch {
+                            prim: anf::BrchOpr::Switch,
+                            args: vec![Atom::Var(t)],
+                            conts: brchs,
+                        }),
+                    }
+                }
+                pattern::ColType::Unknown => {
+                    let (new_mat, hits) = mat.default(j);
+                    let brch =
+                        hits.iter()
+                            .fold(self.normalize_match(&new_mat, decls), |cont, hit| {
+                                anf::Expr::Prim {
+                                    bind: *hit,
+                                    prim: PrimOpr::Move,
+                                    args: vec![Atom::Var(mat.objs[j])],
+                                    cont: Box::new(cont),
+                                }
+                            });
+
+                    brch
+                }
+            }
+        }
     }
 }
 
