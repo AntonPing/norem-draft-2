4ec839c153c78c1e3c843aa750554c1c5b3a6866
Anton Ping <antonping1999@gmail.com>
2024-03-01 18:01:36 +0300

    dev: implement inliner for cps ir

--- src/optimize/inline.rs
+++ src/optimize/inline.rs
@@ -34,13 +34,27 @@ impl InlineScan {
     fn visit_module(&mut self, modl: &Module) {
         let Module { name: _, decls } = modl;
         for decl in decls {
-            self.visit_decl(decl);
+            self.visit_func_decl(decl);
         }
     }
 
-    fn visit_decl(&mut self, decl: &Decl) {
-        let Decl {
+    fn visit_func_decl(&mut self, decl: &FuncDecl) {
+        let FuncDecl {
             func: _,
+            cont,
+            pars,
+            body,
+        } = decl;
+        self.visit_expr(body);
+        pars.iter().for_each(|par| {
+            self.occur_map.remove(par);
+        });
+        self.occur_map.remove(&cont);
+    }
+
+    fn visit_cont_decl(&mut self, decl: &ContDecl) {
+        let ContDecl {
+            cont: _,
             pars,
             body,
         } = decl;
@@ -52,48 +66,50 @@ impl InlineScan {
 
     fn visit_expr(&mut self, expr: &Expr) {
         match expr {
-            Expr::Decls { decls, cont } => {
-                self.visit_expr(cont);
-                for decl in decls {
-                    self.visit_decl(decl)
+            Expr::Decls { funcs, conts, body } => {
+                self.visit_expr(body);
+                for decl in funcs {
+                    self.visit_func_decl(decl)
                 }
-                for decl in decls {
+                for decl in conts {
+                    self.visit_cont_decl(decl)
+                }
+                for decl in funcs {
                     let name = decl.func;
                     let res = self.occur_map.remove(&name);
                     if res == Some((1, 1)) {
-                        // todo: some other heuristic
+                        self.inline_mark.insert(name);
+                    }
+                }
+                for decl in conts {
+                    let name = decl.cont;
+                    let res = self.occur_map.remove(&name);
+                    if res == Some((1, 1)) {
                         self.inline_mark.insert(name);
                     }
                 }
             }
             Expr::Prim {
-                bind,
+                bind: _,
                 prim: _,
                 args,
-                cont,
+                rest,
             } => {
-                self.visit_expr(cont);
+                self.visit_expr(rest);
                 args.iter().for_each(|arg| self.visit_atom(arg));
-                self.occur_map.remove(bind);
             }
-            Expr::Call {
-                bind,
-                func,
-                args,
-                cont,
-            } => {
-                self.visit_expr(cont);
+            Expr::Call { func, cont, args } => {
+                self.visit_atom(&Atom::Var(*func));
+                self.visit_atom(&Atom::Var(*cont));
                 args.iter().for_each(|arg| self.visit_atom(arg));
-                if let Atom::Var(func) = func {
-                    let res = self.occur_map.get_mut(&func);
-                    if let Some((n, m)) = res {
-                        *n += 1;
-                        *m += 1;
-                    } else {
-                        self.occur_map.insert(*func, (1, 1));
-                    }
-                }
-                self.occur_map.remove(bind);
+                let res = self.occur_map.get_mut(&func).unwrap();
+                res.1 += 1;
+            }
+            Expr::Jump { cont, args } => {
+                self.visit_atom(&Atom::Var(*cont));
+                args.iter().for_each(|arg| self.visit_atom(arg));
+                let res = self.occur_map.get_mut(&cont).unwrap();
+                res.1 += 1;
             }
             Expr::Ifte {
                 cond: _,
@@ -119,14 +135,16 @@ impl InlineScan {
 
 pub struct InlinePerform {
     inline_mark: HashSet<Ident>,
-    map: HashMap<Ident, Decl>,
+    func_map: HashMap<Ident, FuncDecl>,
+    cont_map: HashMap<Ident, ContDecl>,
 }
 
 impl InlinePerform {
     pub fn run(modl: Module, mark: HashSet<Ident>) -> Module {
         let mut pass = InlinePerform {
             inline_mark: mark,
-            map: HashMap::new(),
+            func_map: HashMap::new(),
+            cont_map: HashMap::new(),
         };
         pass.visit_module(modl)
     }
@@ -136,81 +154,132 @@ impl InlinePerform {
 
         decls.iter().for_each(|decl| {
             if self.inline_mark.contains(&decl.func) {
-                self.map.insert(decl.func, decl.clone());
+                self.func_map.insert(decl.func, decl.clone());
             }
         });
 
-        let decls: Vec<Decl> = decls
+        let decls: Vec<FuncDecl> = decls
             .into_iter()
-            .map(|decl| self.visit_decl(decl))
+            .map(|decl| self.visit_func_decl(decl))
             .collect();
 
         Module { name, decls }
     }
 
-    fn visit_decl(&mut self, decl: Decl) -> Decl {
-        let Decl { func, pars, body } = decl;
+    fn visit_func_decl(&mut self, decl: FuncDecl) -> FuncDecl {
+        let FuncDecl {
+            func,
+            cont,
+            pars,
+            body,
+        } = decl;
         let body = self.visit_expr(body);
-        Decl { func, pars, body }
+        FuncDecl {
+            func,
+            cont,
+            pars,
+            body,
+        }
+    }
+
+    fn visit_cont_decl(&mut self, decl: ContDecl) -> ContDecl {
+        let ContDecl { cont, pars, body } = decl;
+        let body = self.visit_expr(body);
+        ContDecl { cont, pars, body }
     }
 
     fn visit_expr(&mut self, expr: Expr) -> Expr {
         match expr {
-            Expr::Decls { decls, cont } => {
-                let decls: Vec<Decl> = decls
+            Expr::Decls { funcs, conts, body } => {
+                for decl in funcs.iter() {
+                    if self.inline_mark.contains(&decl.func) {
+                        self.func_map.insert(decl.func, decl.clone());
+                    }
+                }
+                for decl in conts.iter() {
+                    if self.inline_mark.contains(&decl.cont) {
+                        self.cont_map.insert(decl.cont, decl.clone());
+                    }
+                }
+                let funcs: Vec<FuncDecl> = funcs
                     .into_iter()
-                    .map(|decl| {
-                        if self.inline_mark.contains(&decl.func) {
-                            self.map.insert(decl.func, decl.clone());
-                        }
-                        decl
-                    })
+                    .map(|decl| self.visit_func_decl(decl))
+                    .collect();
+                let conts: Vec<ContDecl> = conts
+                    .into_iter()
+                    .map(|decl| self.visit_cont_decl(decl))
                     .collect();
 
-                let decls: Vec<Decl> = decls
-                    .into_iter()
-                    .map(|decl| self.visit_decl(decl))
-                    .collect();
+                let body = Box::new(self.visit_expr(*body));
 
-                let cont = Box::new(self.visit_expr(*cont));
-
-                if decls.is_empty() {
-                    *cont
+                if funcs.is_empty() && conts.is_empty() {
+                    *body
                 } else {
-                    Expr::Decls { decls, cont }
+                    Expr::Decls { funcs, conts, body }
                 }
             }
             Expr::Prim {
                 bind,
                 prim,
                 args,
-                cont,
+                rest,
             } => {
-                let cont = Box::new(self.visit_expr(*cont));
+                let rest = Box::new(self.visit_expr(*rest));
                 Expr::Prim {
                     bind,
                     prim,
                     args,
-                    cont,
+                    rest,
                 }
             }
-            Expr::Call {
-                bind,
-                func,
-                args,
-                cont,
-            } => {
-                let cont = Box::new(self.visit_expr(*cont));
-                if let Atom::Var(func) = func {
-                    if let Some(decl) = self.map.remove(&func) {
-                        return inline_call(decl, bind, func, args, *cont);
-                    }
+            Expr::Call { func, cont, args } => {
+                if let Some(decl) = self.func_map.remove(&func) {
+                    let FuncDecl {
+                        func: func2,
+                        cont: cont2,
+                        pars,
+                        body,
+                    } = decl;
+                    assert_eq!(func, func2);
+                    assert_eq!(args.len(), pars.len());
+                    pars.into_iter().zip(args.into_iter()).fold(
+                        // this have to be const-folded!
+                        Expr::Prim {
+                            bind: cont2,
+                            prim: PrimOpr::Move,
+                            args: vec![Atom::Var(cont)],
+                            rest: Box::new(body),
+                        },
+                        |rest, (par, arg)| Expr::Prim {
+                            bind: par,
+                            prim: PrimOpr::Move,
+                            args: vec![arg],
+                            rest: Box::new(rest),
+                        },
+                    )
+                } else {
+                    Expr::Call { func, cont, args }
                 }
-                Expr::Call {
-                    bind,
-                    func,
-                    args,
-                    cont,
+            }
+            Expr::Jump { cont, args } => {
+                if let Some(decl) = self.cont_map.remove(&cont) {
+                    let ContDecl {
+                        cont: cont2,
+                        pars,
+                        body,
+                    } = decl;
+                    assert_eq!(cont, cont2);
+                    assert_eq!(args.len(), pars.len());
+                    pars.into_iter()
+                        .zip(args.into_iter())
+                        .fold(body, |rest, (par, arg)| Expr::Prim {
+                            bind: par,
+                            prim: PrimOpr::Move,
+                            args: vec![arg],
+                            rest: Box::new(rest),
+                        })
+                } else {
+                    Expr::Jump { cont, args }
                 }
             }
             Expr::Ifte {
@@ -241,106 +310,21 @@ impl InlinePerform {
     }
 }
 
-fn continue_with(expr: Expr, hole: Ident, rest: Expr) -> Expr {
-    match expr {
-        Expr::Decls { decls, cont } => {
-            let cont = Box::new(continue_with(*cont, hole, rest));
-            Expr::Decls { decls, cont }
-        }
-        Expr::Prim {
-            bind,
-            prim,
-            args,
-            cont,
-        } => {
-            let cont = Box::new(continue_with(*cont, hole, rest));
-            Expr::Prim {
-                bind,
-                prim,
-                args,
-                cont,
-            }
-        }
-        Expr::Call {
-            bind,
-            func,
-            args,
-            cont,
-        } => {
-            let cont = Box::new(continue_with(*cont, hole, rest));
-            Expr::Call {
-                bind,
-                func,
-                args,
-                cont,
-            }
-        }
-        Expr::Ifte {
-            cond,
-            args,
-            trbr,
-            flbr,
-        } => {
-            let trbr = Box::new(continue_with(*trbr, hole, rest.clone()));
-            let flbr = Box::new(continue_with(*flbr, hole, rest));
-            Expr::Ifte {
-                cond,
-                args,
-                trbr,
-                flbr,
-            }
-        }
-        Expr::Switch { arg, brchs, dflt } => {
-            let brchs = brchs
-                .into_iter()
-                .map(|(i, brch)| (i, continue_with(brch, hole, rest.clone())))
-                .collect();
-            let dflt = dflt.map(|dflt| Box::new(continue_with(*dflt, hole, rest)));
-            Expr::Switch { arg, brchs, dflt }
-        }
-        Expr::Retn { res } => Expr::Prim {
-            bind: hole,
-            prim: PrimOpr::Move,
-            args: vec![res],
-            cont: Box::new(rest),
-        },
-    }
-}
-
-fn inline_call(decl: Decl, bind: Ident, func: Ident, args: Vec<Atom>, cont: Expr) -> Expr {
-    let Decl {
-        func: func2,
-        pars,
-        body,
-    } = decl;
-    assert_eq!(func, func2);
-    assert_eq!(args.len(), pars.len());
-    pars.into_iter().zip(args.into_iter()).fold(
-        continue_with(body, bind, cont),
-        |tail, (par, arg)| Expr::Prim {
-            bind: par,
-            prim: PrimOpr::Move,
-            args: vec![arg],
-            cont: Box::new(tail),
-        },
-    )
-}
-
 #[test]
 #[ignore = "just to see result"]
 fn inline_test() {
     let s = r#"
 module Test where
-fn g(x) begin
-    decl
-        fn f(x) begin
-        return x; 
-        end
+func test(k):
+    decls
+        func f(k1, x1):
+            call g(k1, x1);
+        func g(k2, x2):
+            let r = @iadd(x2, x2);
+            jump k2(r);
     in
-        let y = f(42);
-        return y;
+        call f(k, 42);
     end
-end
 "#;
     let expr = super::parser::parse_module(s).unwrap();
     println!("{}", expr);
--- src/optimize/mod.rs
+++ src/optimize/mod.rs
@@ -1,6 +1,6 @@
 pub mod anf;
 // pub mod closure;
-// pub mod inline;
+pub mod inline;
 // pub mod normalize;
 pub mod optimize;
 pub mod parser;
